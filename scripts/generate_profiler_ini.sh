6c11e111e12ac936b1e062206f9cec7cfc4a70e1
flo <flo@cis.lmu.de>
2015-04-30 16:17:54 +0200

    added backend setting and marked old settings as deprecated

--- scripts/generate_profiler_ini.sh
+++ scripts/generate_profiler_ini.sh
@@ -3,16 +3,21 @@
 if test -z $1; then
     exit 1;
 fi
+if test -z $2; then
+    exit 1;
+fi
 
 cat > $1 <<EOF
 #
 # profiler.ini
-# automatically generated on `date` *do not edit *
+# automatically generated on `date` *do not edit*
 #
+backend = $2
+
+# DEPRECATED SETTINGS
 dbURL = jdbc:mysql://alpha.cis.uni-muenchen.de:3306/ProfilerWebService
 username = pws
 password = pws1314
 default_quota = 100
 backend_home = /mounts/Users/student/finkf/uni/profiler/tomcat/postcorrection_backend
-backend = /home/flo/devel/clarin-ressources/backend
 EOF
