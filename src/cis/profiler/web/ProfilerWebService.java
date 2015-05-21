483bb2486f51d31062a7048a2b18314380a09492
flo <flo@cis.lmu.de>
2015-05-21 14:08:17 +0200

    removed check quota

--- src/cis/profiler/web/ProfilerWebService.java
+++ src/cis/profiler/web/ProfilerWebService.java
@@ -199,11 +199,6 @@ public class ProfilerWebService implements ProfilerWebServiceSkeletonInterface {
 
         // old interface
         @Override
-        public CheckQuotaResponse checkQuota(CheckQuotaRequest x) {
-                log(Level.SEVERE, "called checkQuota()");
-                return null;
-        }
-        @Override
         public GetTransactionsResponse getTransactions(GetTransactionsRequest x) {
                 log(Level.SEVERE, "called getTransactions()");
                 return null;
