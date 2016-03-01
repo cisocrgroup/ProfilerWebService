92f0c19dd4cc8f8c62bd445c926b1eb0434f889d
flo <finkf@cis.uni-muenchen.de>
2016-03-01 12:57:35 +0100

    add error message

--- scripts/generate_profiler_ini.sh
+++ scripts/generate_profiler_ini.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 if test -z $1; then
+    echo "Missing out file"
     exit 1;
 fi
 if test -z $2; then
+    echo "Missing backend"
     exit 1;
 fi
 
 if test -z $3; then
+    echo "Missing profiler"
     exit 1;
 fi
 
