76ffbac165b2ecc52dcae0ba31bc9d793d53bfb5
flo <finkf@cis.uni-muenchen.de>
2016-03-01 14:58:38 +0100

    remove deprecated settings from ini file

--- scripts/generate_profiler_ini.sh
+++ scripts/generate_profiler_ini.sh
@@ -21,10 +21,4 @@ cat > $1 <<EOF
 #
 backend = $2
 profiler = $3
-# DEPRECATED SETTINGS
-dbURL = jdbc:mysql://alpha.cis.uni-muenchen.de:3306/ProfilerWebService
-username = pws
-password = pws1314
-default_quota = 100
-backend_home = /mounts/Users/student/finkf/uni/profiler/tomcat/postcorrection_backend
 EOF
